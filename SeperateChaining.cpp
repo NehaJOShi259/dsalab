910666dacbff1ca5778952365b85abd7e995fce9
NehaJOShi259 <nehajoshi6350@gmail.com>
2025-03-30 14:04:31 +0530

    Update SeperateChaining.cpp

--- SeperateChaining.cpp
+++ SeperateChaining.cpp
@@ -5,24 +5,29 @@ using namespace std;
 class HashTable {
 private:
     vector<list<string>> table;
+
     int hashFunction(const string& word) {
         int hash = 0;
-        for (char c : word) hash += c;
+        for (char c : word) {
+            hash += c;
+        }
         return hash % table.size();
     }
 
 public:
     HashTable(int size) : table(size) {}
-    
+
     void insert(const string& word) {
         table[hashFunction(word)].push_back(word);
     }
 
     void display() {
-        for (int i = 0; i < table.size(); i++) {
+        for (int i = 0; i < table.size(); ++i) {
             cout << "Bucket " << i << ": ";
-            for (const auto& word : table[i]) cout << word << " -> ";
-            cout << "NULL\n";
+            for (const string& word : table[i]) {
+                cout << word << " -> ";
+            }
+            cout << "NULL" << endl;
         }
     }
 };
@@ -30,22 +35,20 @@ public:
 int main() {
     HashTable hashTable(10);
     int choice;
-    string word;
-    
+    string key;
+
     while (true) {
-        cout << "1. Add\n2. Display\n3. Exit\nYour choice: ";
+        cout << "1. Add\n2. Display\n3. Exit\nChoice: ";
         cin >> choice;
-        
+
         if (choice == 1) {
-            cout << "Enter word: ";
-            cin >> word;
-            hashTable.insert(word);
+            cout << "Enter element: ";
+            cin >> key;
+            hashTable.insert(key);
         } else if (choice == 2) {
             hashTable.display();
-        } else if (choice == 3) {
-            break;
         } else {
-            cout << "Invalid choice. Try again.\n";
+            break;
         }
     }
     return 0;
